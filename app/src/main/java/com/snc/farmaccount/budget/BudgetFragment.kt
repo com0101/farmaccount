201cb20ff75fd9a1d2e68e901a618de07b442097
com0101 <jjonerepulic0101@gmail.com>
2019-09-22 19:34:25 +0800

    change budget UI position

--- app/src/main/java/com/snc/farmaccount/budget/BudgetFragment.kt
+++ app/src/main/java/com/snc/farmaccount/budget/BudgetFragment.kt
@@ -2,17 +2,21 @@ package com.snc.farmaccount.budget
 
 
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.snackbar.Snackbar
+import com.snc.farmaccount.ApplicationContext
 import com.snc.farmaccount.MainViewModel
 import com.snc.farmaccount.R
 import com.snc.farmaccount.`object`.Budget
@@ -240,9 +244,13 @@ class BudgetFragment : Fragment() {
             dialog.dismiss()
         }
 
-
-
     }
+//    fun showSoftKeyboard(view: View) {
+//        if (view.requestFocus()) {
+//            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+//        }
+//    }
 
 
 }
